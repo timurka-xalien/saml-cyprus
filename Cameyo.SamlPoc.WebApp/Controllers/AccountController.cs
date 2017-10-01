7972da98f12462d7bd0405e4a66447613c917009
xalien <atkaranda@gmail.com>
2017-10-01 22:33:54 +0300

    Initial commit

---
+++ Cameyo.SamlPoc.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Configuration;
+using System.Web.Mvc;
+
+using ComponentSpace.SAML2;
+
+namespace Cameyo.SamlPoc.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult SingleSignOn()
+        {
+            // To login at the service provider, initiate single sign-on to the identity provider (SP-initiated SSO).
+            string partnerIdP = WebConfigurationManager.AppSettings[AppSettings.PartnerIdP];
+            SAMLServiceProvider.InitiateSSO(Response, null, partnerIdP);
+
+            return new EmptyResult();
+        }
+    }
+}
