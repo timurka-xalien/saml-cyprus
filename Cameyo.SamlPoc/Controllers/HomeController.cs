6c49ec3e10a929986eab1ddedaceb5abfbf67dac
xalien <atkaranda@gmail.com>
2017-10-15 19:01:29 +0300

    Configure SSO with AGF

--- Cameyo.SamlPoc/Controllers/HomeController.cs
+++ Cameyo.SamlPoc/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
